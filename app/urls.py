861e702d7dddc23a8dc9590ac2bf523e045b27af
ahu <1540303002@qq.com>
2020-07-20 14:09:42 +0800

    实现A等级的安全级别2

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from app import views
 
 app_name = 'app'
 urlpatterns = [
-    path('',views.index,name='index')  # 配置访问首页的路由
+    path('',views.index,name='index'),  # 配置访问首页的路由
+    path('safe/',views.safeA,name='safe')  # 配置访问安全等级A的路由
 ]
