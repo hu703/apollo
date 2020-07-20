f57da162b7c193238c5f7b62cdb3cf47950595fc
ahu <1540303002@qq.com>
2020-07-20 10:02:17 +0800

    实现A等级的安全级别

--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('hello world')
+
+def safeA(request):
+    return HttpResponse('实现了A级别的安全等级')
